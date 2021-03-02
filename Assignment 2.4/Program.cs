5d5a10cb404d138f73ad6c76cb04e5619c61ad70
Qianxi Liu <qianxiliu@usf.edu>
2021-03-01 23:06:34 -0500

    Add project files.

---
+++ Assignment 2.4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
